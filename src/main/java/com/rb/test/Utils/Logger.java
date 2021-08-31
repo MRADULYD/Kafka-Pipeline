41fd42a8a7b18592b78d468da72cc9e50cfa87b2
MRADULYD <49901753+MRADULYD@users.noreply.github.com>
2021-08-31 18:47:17 +0530

    Create Logger.java

---
+++ src/main/java/com/rb/test/Utils/Logger.java
@@ -0,0 +1,9 @@
+package com.rb.test.Utils;
+
+import com.rb.test.TestApplication;
+import org.slf4j.LoggerFactory;
+
+public class Logger {
+
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TestApplication.class);
+}
